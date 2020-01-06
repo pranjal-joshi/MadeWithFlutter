bd44d995f69c5fa30a3961e8a4cd5d4b8c6c5271
Pranjal Joshi <joshi.pranjal5@gmail.com>
2020-01-06 20:25:59 +0530

    Dark mode compatibility added

--- made_with_flutter/lib/onboarding_screen/onboarding_screen.dart
+++ made_with_flutter/lib/onboarding_screen/onboarding_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:intro_views_flutter/Models/page_view_model.dart';
 import 'package:intro_views_flutter/intro_views_flutter.dart';
+import 'package:made_with_flutter/helper.dart';
 import 'package:made_with_flutter/login_screen/login_screen.dart';
 import 'package:page_transition/page_transition.dart';
 
@@ -11,7 +12,7 @@ import '../global_style.dart';
 
 class OnboardingScreen extends StatelessWidget {
 
-  final onboardingPages = [
+  final onboardingPagesLight = [
     PageViewModel(
       pageColor: Colors.blue[600],
       mainImage: Padding(
@@ -140,14 +141,146 @@ class OnboardingScreen extends StatelessWidget {
     ),
   ];
 
+  final onboardingPagesDark = [
+    PageViewModel(
+      pageColor: Colors.blue[600].withOpacity(0.5),
+      mainImage: Padding(
+        padding: EdgeInsets.all(marginLarge),
+        child: SvgPicture.asset(
+          "lib/assets/images/ob1.svg",
+          alignment: Alignment.center,
+          fit: BoxFit.fitWidth,
+        ),
+      ),
+      title: Text("Ideas!"),
+      body: Text("Always think something new when it comes to the UI/UX."),
+      titleTextStyle: TextStyle(
+        fontFamily: defaultFont,
+        fontSize: fontSizeLarge*2,
+        letterSpacing: 1.0,
+        color: Colors.white
+      ),
+      bodyTextStyle: TextStyle(
+        fontFamily: defaultFont,
+        fontSize: fontSizeLarge,
+        letterSpacing: 1.0,
+        color: Colors.white
+      )
+    ),
+    PageViewModel(
+      pageColor: Colors.teal[600].withOpacity(0.5),
+      mainImage: Padding(
+        padding: EdgeInsets.all(marginLarge),
+        child: SvgPicture.asset(
+          "lib/assets/images/ob2.svg",
+          alignment: Alignment.center,
+          fit: BoxFit.fitWidth,
+        ),
+      ),
+      title: Text("Imagine!"),
+      body: Text("Draw a picture in your mind\n\nHow your screen should look like?"),
+      titleTextStyle: TextStyle(
+        fontFamily: defaultFont,
+        fontSize: fontSizeLarge*2,
+        letterSpacing: 1.0,
+        color: Colors.white
+      ),
+      bodyTextStyle: TextStyle(
+        fontFamily: defaultFont,
+        fontSize: fontSizeLarge,
+        letterSpacing: 1.0,
+        color: Colors.white
+      )
+    ),
+    PageViewModel(
+      pageColor: Colors.indigo[600].withOpacity(0.5),
+      mainImage: Padding(
+        padding: EdgeInsets.all(marginLarge),
+        child: SvgPicture.asset(
+          "lib/assets/images/ob3.svg",
+          alignment: Alignment.center,
+          fit: BoxFit.fitWidth,
+        ),
+      ),
+      title: Text("Innovate!"),
+      body: Text("Tinker with the building blocks to achieve smooth and unique user experience!"),
+      titleTextStyle: TextStyle(
+        fontFamily: defaultFont,
+        fontSize: fontSizeLarge*2,
+        letterSpacing: 1.0,
+        color: Colors.white
+      ),
+      bodyTextStyle: TextStyle(
+        fontFamily: defaultFont,
+        fontSize: fontSizeLarge,
+        letterSpacing: 1.0,
+        color: Colors.white
+      )
+    ),
+    PageViewModel(
+      pageColor: Colors.purple[600].withOpacity(0.5),
+      mainImage: Padding(
+        padding: EdgeInsets.all(marginLarge),
+        child: SvgPicture.asset(
+          "lib/assets/images/ob4.svg",
+          alignment: Alignment.center,
+          fit: BoxFit.fitWidth,
+        ),
+      ),
+      title: Text("Implement!"),
+      body: Text("Start Working!\n\nCode-Check-Modify-Repeate"),
+      titleTextStyle: TextStyle(
+        fontFamily: defaultFont,
+        fontSize: fontSizeLarge*2,
+        letterSpacing: 1.0,
+        color: Colors.white
+      ),
+      bodyTextStyle: TextStyle(
+        fontFamily: defaultFont,
+        fontSize: fontSizeLarge,
+        letterSpacing: 1.0,
+        color: Colors.white
+      )
+    ),
+    PageViewModel(
+      pageColor: Colors.orange[400].withOpacity(0.5),
+      mainImage: Padding(
+        padding: EdgeInsets.all(marginLarge),
+        child: SvgPicture.asset(
+          "lib/assets/images/ob5.svg",
+          alignment: Alignment.center,
+          fit: BoxFit.fitWidth,
+        ),
+      ),
+      title: Text("Improvise!"),
+      body: Text("Review and Share your layout with people.\nLearn from their feedback!"),
+      titleTextStyle: TextStyle(
+        fontFamily: defaultFont,
+        fontSize: fontSizeLarge*2,
+        letterSpacing: 1.0,
+        color: Colors.white
+      ),
+      bodyTextStyle: TextStyle(
+        fontFamily: defaultFont,
+        fontSize: fontSizeLarge,
+        letterSpacing: 1.0,
+        color: Colors.white
+      ),
+      
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
+
+    FlutterHelper flutterHelper = FlutterHelper(context);
    
     return Scaffold(
+      backgroundColor: flutterHelper.isDarkModeActive() ? Colors.black : Colors.white,
       body: IntroViewsFlutter(
-        onboardingPages,
+        flutterHelper.isDarkModeActive() ? onboardingPagesDark : onboardingPagesLight,
         doneButtonPersist: false,
-        showSkipButton: true,
+        //showSkipButton: false,
         onTapDoneButton: () {
           Navigator.pushReplacement(context, PageTransition(
           child: LoginScreen(),
